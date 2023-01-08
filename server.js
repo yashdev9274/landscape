bbbf72b79bfef090d49543adabd30329eab60294
Yash Dewasthale <72986960+yashdev9274@users.noreply.github.com>
2023-01-08 20:02:30 +0530

    updated the serverjs file as per the  issue no #2963

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 #!/usr/bin/env node
-const req = require('https').request('https://raw.githubusercontent.com/cncf/landscapeapp/master/netlify/server.js', function(res) {
-    process.on('SIGINT', function() {
-        console.log("Caught interrupt signal. Exiting...");
-        process.exit();
-    });
-    let script = '';
-    res.on('data', (chunk) => script += chunk);
-    res.on('end', () => eval(script));
+const https = require('https');
+
+const req = https.request('https://raw.githubusercontent.com/cncf/landscapeapp/master/netlify/server.js', function(res) {
+  process.on('SIGINT', function() {
+    console.log("Caught interrupt signal. Exiting...");
+    process.exit();
+  });
+
+  let script = '';
+  res.on('data', (chunk) => script += chunk);
+  res.on('end', () => {
+    try {
+      setTimeout(() => {
+        console.error('Script execution timed out');
+        process.exit(1);
+      }, 1000); // Set timeout to 1 second
+      eval(script);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  });
 });
+
+req.on('error', (error) => {
+  console.error(error);
+  process.exit(1);
+});
+
 req.end();
